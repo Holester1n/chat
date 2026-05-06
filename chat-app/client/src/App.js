2d53583796ecc40b649b584a9ada106cce0bc51e
Holester1n <aleksoch2005@gmail.com>
2026-05-06 21:19:22 +0700

    фикс сабмодуля

---
+++ chat-app/client/src/App.js
@@ -0,0 +1,116 @@
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3000");
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState("");
+  const [isRegister, setIsRegister] = useState(false);
+  
+  useEffect(() => {
+    socket.on("load_messages", (msgs) => {
+      setMessages(msgs);
+    });
+
+    socket.on("receive_message", (msg) => {
+      setMessages((prev) => [...prev, msg]);
+    });
+
+    return () => {
+    socket.off("load_messages");
+    socket.off("receive_message");
+    };
+  }, []);
+
+  const sendMessage = () => {
+    if (!message) return;
+
+    const msg = {
+      text: message,
+      id: Date.now(),
+      username: username
+    };
+
+    socket.emit("send_message", msg);
+    setMessage("");
+  };
+
+  if (!isLoggedIn) {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>{isRegister ? "Register" : "Login"}</h2>
+
+      <input
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        className="inputPlace"
+      />
+
+      <input
+        placeholder="Password"
+        type="password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+        className="inputPlace"
+      />
+
+      <button className="button" onClick={async () => {
+        const url = isRegister ? "/register" : "/login";
+        const res = await fetch(`http://localhost:3000${url}`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ username, password })
+        });
+        const data = await res.json();
+        if (data.error) return alert(data.error);
+
+        if (!isRegister) {
+          setToken(data.token);
+        }
+
+        setIsLoggedIn(true);
+      }}>
+        {isRegister ? "Register" : "Login"}
+      </button>
+      
+      <p style={{ marginTop: 10 }}>
+        {isRegister ? "Already have an account?" : "No account?"}{" "}
+        <button className="button" onClick={() => setIsRegister(!isRegister)}>
+          {isRegister ? "Login" : "Register"}
+        </button>
+      </p>
+    </div>
+  );
+}
+
+  return (
+    
+    <div style={{ padding: 20 }}>
+      <h2>Chat</h2>
+
+      <div>
+        {messages.map((m) => (
+          <div key={m.id} className="username">
+            <strong>{m.username}:</strong>
+            {m.text}
+          </div>
+        ))}
+      </div>
+
+      <input
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        className="inputPlace"
+      />
+      <button className="button" onClick={sendMessage}>Send</button>
+    </div>
+  );
+}
+
+export default App;
